32def1e6b17d5be08b89d2eb165752e62e08334c
gigiali <ali.je@husky.neu.edu>
2021-04-07 21:36:04 -0400

    ugly cards for recipes

--- web-ui/src/Recipes/List.js
+++ web-ui/src/Recipes/List.js
@@ -2,63 +2,49 @@ import { Row, Col, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-function RecipesList({recipes}) {
-    let rows = recipes.map((recipe) => (
-      <tr key={recipe.id}>
-        <td>{recipe.api_id}</td>
-        <td>{recipe.rep["strMeal"]}</td>
-            <td>
-                <Button variant="primary" type="submit">
-            {/* FIXME : implement saving  */}
-            {/* onclick=function(recipe.api_id) */}
-            Save
-          </Button>
-            </td>
-        </tr>
-    ));
+function RecipesList({ recipes }) {
 
-{/* <div class="row">
-  <%= for event <- @events do %>
-    <div class="col-sm-4 card">
-        <h3 class="card-title"><%= event.name %> </h3>
-        <h6 class="card-title">Hosted by: <%= event.usr.name %></h6>
-        <h6 class="card-title">Date: <%= event.when %> </h6>
-        <p class="card-body"><%= event.description %></p>
-          <%=link "view", to: Routes.event_path(@conn, :show, event) %>
+  let cards = recipes.map((recipe) => (
+    <div class="card-group"> 
+    <div class="col-sm-10 card">
+      {/* <img class="card-img-top" src="pic"></img> */}
+      <h3 class="card-title">{recipe.id}</h3>
+      <h6 class="card-title">{recipe.api_id}</h6>
+      <div class="card-body">
+        <div>{recipe.rep["strMeal"]}</div>
+        <div><Button variant="primary">save</Button></div>
+        </div>
+        </div>
     </div>
-  <% end %>
-</div> */}
+  ));
 
-return (
+
+
+  return (
     <div>
-      <Row>
-        <Col>
+      {/* <Row>
+        <Col> */}
           <h2>Search Results</h2>
           <p>
             <Link to="/recipes">
               Go back to search
             </Link>
           </p>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Save</th>
-              </tr>
-            </thead>
-            <tbody>
-              { rows }
-            </tbody>
-          </table>
-        </Col>
-      </Row>
+          {/* <table className="table table-striped">
+            
+            <tbody> */}
+            <div class="card-deck">{cards}</div>
+          
+          {/* </tbody>
+          </table> */}
+        {/* </Col>
+      </Row> */}
     </div>
   );
 
 }
 
-function state2props({recipes, recipe_form}) {
+function state2props({ recipes, recipe_form }) {
   return { recipes, recipe_form };
 }
 
