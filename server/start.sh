3dbd0f17f30289c18b389cde1d8a25c1218137fa
gigiali <ali.je@husky.neu.edu>
2021-04-10 12:09:39 -0400

    start script

--- server/start.sh
+++ server/start.sh
@@ -4,4 +4,4 @@ export NODEBIN='pwd'/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
 export DATABASE_URL=ecto://kitchen:postgres@localhost/server_dev
 # TODO change this
-_build/prod/rel/events_app_spa/bin/events_app_spa start
+_build/prod/rel/server/bin/server start
